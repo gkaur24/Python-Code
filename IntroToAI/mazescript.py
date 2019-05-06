a511bd30a7ad3264e580232e251586b2df52f7aa
Gurpreet Kaur <30677221+gkaur24@users.noreply.github.com>
2019-05-06 12:55:29 -0400

    Add files via upload

---
+++ IntroToAI/mazescript.py
@@ -0,0 +1,10 @@
+import numpy as np
+import mazeGenerator
+from tempfile import TemporaryFile
+count = 2
+while count < 51:
+    grid = np.zeros([101, 101], dtype=int)
+    mazeGenerator.make_passages(0, 0, grid)
+    outfile = TemporaryFile()
+    np.save('/Users/kevinpatel/Desktop/mzs/grid'+str(count), grid)
+    count += 1
